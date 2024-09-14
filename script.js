8642450b19b3c29ee730f6d5b3f391540c2ce5b4
ck-js <ckentertainmentinfo@gmail.com>
2024-09-14 21:34:50 +0700

    implement insert tree method that takes a value and first checks if the root of the tree is null if true it inserts new node with passed data as value for a new node at root. Otherwise it means the tree already has nodes so we define a recursive function to traverse the tree nodes by comparing the insert value with the value of the current node which is initially the root. Our base case is when the current node is null meaning we have traversed the tree and found the appropriate position to create the new node. When base case is not met we compare the data to data of current node then either pass the left or right of the current node as the new current node of the next recursive call until base case is met.

--- script.js
+++ script.js
@@ -36,6 +36,58 @@ class Tree {
     self.indexOf(value) === index);
     return uniqueArray
     }
+    insert(data) {
+// if the bst is empty
+      if (this.root === null) {
+        this.root = new Node(data);
+        return;
+      }
+      // if the bst is not empty
+const insertRecursively = (node,data) => {
+  // console.log(
+  //   `Calling insertRecursively with node: 
+  //   ${node ? node.data : 'null'} and data: 
+  //   ${data}`);
+  // console.log(node ? true : false);
+  console.log(`${node ? node.data : 'null'}`);
+  if (node === null) return new Node(data)
+    if (data < node.data) {
+      node.left = insertRecursively(node.left, data)
+
+    } else if (data > node.data) {
+      node.right = insertRecursively(node.right, data)
+      
+    }
+    
+    return node
+}
+this.root = insertRecursively(this.root, data)
+
+
+
+
+      // let current = this.root;
+      // while (true) {
+      //   if (data < current.data) {
+      //     if (current.left === null) {
+      //       current.left = new Node(data);
+      //       return;
+      //     }
+      //     current = current.left;
+      //   } else {
+      //     if (current.right === null) {
+      //       current.right = new Node(data);
+      //       return;
+      //     }
+      //     current = current.right;
+      //   }
+      // }
+
+
+      
+    
+
+    }
 }
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -55,12 +107,13 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
 
 const array1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-const array2 = [1,2,3,4]
+const array2 = [1,3,4,5,6]
 
 const tree1 = new Tree(array1)
-console.log(tree1.sortArrayAscending(array1))
+// console.log(tree1.sortArrayAscending(array1))
 const tree2 = new Tree(array2)
-// console.log(tree2.root)
+tree2.insert(2)
+console.log(tree2.root)
 
-prettyPrint(tree1.root)
-// prettyPrint(tree2.root)
+// prettyPrint(tree1.root)
+prettyPrint(tree2.root)
